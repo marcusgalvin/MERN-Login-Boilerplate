0ecf169c1e01528ecdc0aeadc8cbd9b487c0d967
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-02-09 17:33:40 -0500

    updated mongo connection URI

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { render } from 'react-dom';
+
+//imports
+import Navbar from './components/NavBar'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/Profile'
+import Product from "./components/Product"
+
+
+
+
+
+
+//class
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="main-app">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile} />
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
